7a6b3cadef4c064922c0e8704ac6ae83d04053aa
gmg-907 <medynskyypavlo@gmail.com>
2017-01-09 11:55:59 +0200

    new horizont

--- inner/application/config/config.php
+++ inner/application/config/config.php
@@ -2,7 +2,7 @@
 
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	$config['base_url'] = 'http://programmer/';
+	$config['base_url'] = 'http://komilfo.sufix/';
 
 	/*
 	|--------------------------------------------------------------------------
